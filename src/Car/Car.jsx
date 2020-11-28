f9b5e60905bc5d7efa64ee183be38b5b650ffc5a
Vitalii-Kutsuruk <kutsurukvet@gmail.com>
2020-11-28 19:56:01 +0200

    Add dynamic classes for input field with condition

--- src/Car/Car.jsx
+++ src/Car/Car.jsx
@@ -3,6 +3,19 @@ import './Car.css'
 
 
 const Car = props => {
+
+    const inputClasses = ['input']
+
+    if(props.name !== '') {
+        inputClasses.push('green')
+    } else {
+        inputClasses.push('red')
+    }
+
+    if(props.name.length > 4) {
+        inputClasses.push('bold')
+    }
+
     return(
         <div className='Car'>
             <h3>Car name: {props.name} {props.model}</h3>
@@ -11,7 +24,11 @@ const Car = props => {
                     Year: {props.year}
                 </strong>
             </p>
-            <input onChange={props.onChangeName}  value={props.name} />
+            <input 
+                onChange={props.onChangeName}  
+                value={props.name} 
+                className={inputClasses.join(' ')}
+            />
             <button onClick={props.onDelete}>Delete</button>
         </div>
     )
