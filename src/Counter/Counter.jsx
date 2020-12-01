6709a1c65cde06a5d6d9af806fea0d667a961aa0
Vitalii-Kutsuruk <kutsurukvet@gmail.com>
2020-12-01 14:25:25 +0200

    Add React.Fragment

--- src/Counter/Counter.jsx
+++ src/Counter/Counter.jsx
@@ -19,10 +19,20 @@ export default class Counter extends Component {
     }
 
     render() {
+        return(
+            <React.Fragment>
+                <h3>Counter: {this.state.counter}</h3>
+                <button onClick={this.addCounter}>+</button>
+                <button onClick={this.minusCounter}>-</button>
+            </React.Fragment>
+        )
+    }
+
+    /* render() {
         return[
             <h3 key={'1'}>Counter: {this.state.counter}</h3>,
             <button key={'2'} onClick={this.addCounter}>+</button>,
             <button key={'3'} onClick={this.minusCounter}>-</button>,
         ]
-    }
+    } */
 }
